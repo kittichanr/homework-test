44bf37aafcd558c8f16202f73c79b87280c66d0c
petchkubbb <kittichan@lmwn.com>
2024-07-04 09:15:53 +0700

    add put delete method

--- backend/index.js
+++ backend/index.js
@@ -59,9 +59,49 @@ app.post('/todo-list', (req, res) => {
     })
 })
 
-// PUT
-// DELETE
+
+app.put('/todo-list/:id', async (req, res) => {
+    let id = req.params.id
+    let updateTodo = req.body
+
+    const selectedIndex = todoLists.findIndex(todo => todo.id == id)
+    if (selectedIndex == -1) {
+        res.status(404).json(`NOT FOUND ID: ${id}`)
+        return
+    }
+
+    todoLists[selectedIndex].title = updateTodo.title || todoLists[selectedIndex].title
+    todoLists[selectedIndex].description = updateTodo.description || todoLists[selectedIndex].description
+
+    if (updateTodo.status != STATUS.IN_PROGRESS && updateTodo.status != STATUS.COMPLETED) {
+        res.status(400).json(`status should be IN_PROGRESS or COMPLETED`)
+        return
+    }
+    todoLists[selectedIndex].status = updateTodo.status || todoLists[selectedIndex].status
+    res.json({
+        message: 'update todo complete!',
+        data: todoLists[selectedIndex]
+    })
+})
+
+
+app.delete('/todo-list/:id', async (req, res) => {
+    let id = req.params.id
+
+    const selectedIndex = todoLists.findIndex(todo => todo.id == id)
+    if (selectedIndex == -1) {
+        res.status(404).json(`NOT FOUND ID: ${id}`)
+        return
+    }
+
+    todoLists.splice(selectedIndex, 1)
+
+    res.json({
+        message: 'delete ok',
+        indexDeleted: selectedIndex
+    })
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Start listening on port ${port}`)
 })
